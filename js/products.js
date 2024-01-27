14918f8dd54a31eec75adda471a32874fc32c49b
MiaTexnes <miatex50428@stud.noroff.no>
2024-01-27 20:59:28 +0100

    api-fail

--- js/products.js
+++ js/products.js
@@ -30,7 +30,7 @@ async function getProducts(gender, containerProduct) {
   } catch (error) {
     console.error("Error fetching products:", error);
 
-    const resultsContainer = document.querySelector(containerProduct);
+    const resultsContainer = document.querySelector("#container-product");
     resultsContainer.innerHTML = `<p>Failed to load products. Please try again later.</p>`;
   }
 }
