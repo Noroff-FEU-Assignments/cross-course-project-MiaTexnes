28f42f43ee5fcff4c77391ab6973f917d694bfbc
MiaTexnes <miatex50428@stud.noroff.no>
2024-02-24 19:48:33 +0100

    hamburgler

--- js/allPages.js
+++ js/allPages.js
@@ -33,3 +33,23 @@ function addFavicon(data) {
 for (var i = 0; i < favicons.length; i++) {
     addFavicon(favicons[i]);
 }
+
+const openIcon = document.querySelector("#open");
+const closeIcon = document.querySelector("#close");
+
+if (openIcon && closeIcon) {
+    openIcon.addEventListener("click", function () {
+        openIcon.style.display = "none";
+        document.querySelector(".navbar").style.display = "block";
+        closeIcon.style.display = "block";
+    });
+
+    closeIcon.addEventListener("click", function () {
+        closeIcon.style.display = "none";
+        document.querySelector(".navbar").style.display = "none";
+        openIcon.style.display = "block";
+    });
+}
+
+
+
